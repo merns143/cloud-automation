d29b1b813dfa9d95cbd0991b735ceb0cc40ffde5
Mernante Tullaga <49087021+mtullaga@users.noreply.github.com>
2023-04-18 20:46:00 +0800

    Rebranding to Flutter+

--- public/app.routes.js
+++ public/app.routes.js
@@ -29,7 +29,7 @@
                 }
             },
             params: {
-                title: "Nexysmedia"
+                title: "Flutter+"
             }
         }) 
         .state('app.login', {
